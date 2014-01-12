f32bc53471975f376c0425780391b7b00bb0ce21
Dmitrijs Vrublevskis <dv@tpro.lv>
2014-01-12 17:36:16 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LvougAppWebservice::Application.config.secret_key_base = '288e18afa7d7758944a0c956cacc0bfb48b981183f14723f65490b17b886904db9083a1a3df3698bd436766864331b5b7aeee926aa476304fe26a095a5452569'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LvougAppWebservice::Application.config.session_store :cookie_store, key: '_lvoug-app-webservice_session'
