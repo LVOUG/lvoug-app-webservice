b2a5c3eb89cec5ff9f9d397c9b874f8b393d24b4
Dmitrijs Vrublevskis <dv@tpro.lv>
2014-01-12 17:54:52 +0200

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+user = User.new(email: 'admin@site.com', password: 'password123', password_confirmation: 'password123')
+if user.save
+  puts 'Admin account created.'
+  puts 'Email: admin@site.com'
+  puts 'Password: password123'
+else
+  puts 'ERROR! Admin account are not created!'
+end
