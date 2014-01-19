d86a3019dff466dbef03f073635a8eafae4145e2
Dmitrijs Vrublevskis <d.vrublevskis@gmail.com>
2014-01-19 12:30:55 +0200

    Normalize User edit/create form in admin panel - remove unnecessary fields.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+
+  rails_admin do
+    edit do
+      field :email
+      field :password
+      field :password_confirmation
+    end
+  end
 end
