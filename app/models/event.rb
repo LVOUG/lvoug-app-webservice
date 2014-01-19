5de42d0d2f646e7b4b76a02fb3488b33ec2fd983
Dmitrijs Vrublevskis <d.vrublevskis@gmail.com>
2014-01-19 13:14:05 +0200

    Fix event model - lat and long are not required now.

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,6 @@ class Event < ActiveRecord::Base
   validates :description, presence: true, length: {minimum: 3, maximum: 1000}
   validates :address, presence: true, length: {minimum: 3, maximum: 255}
   validates :event_page, presence: true, length: {minimum: 3, maximum: 255}
-  validates :address_latitude, numericality: true
-  validates :address_longitude, numericality: true
+  validates :address_latitude, numericality: true, allow_nil: true
+  validates :address_longitude, numericality: true, allow_nil: true
 end
