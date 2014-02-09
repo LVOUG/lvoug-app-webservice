a99a1153bcca42fca8f0e8d5e1d2a2368bca79d7
Dmitrijs Vrublevskis <d.vrublevskis@gmail.com>
2014-02-09 13:50:14 +0200

    Add updated_at & created_at to articles

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,3 +1,4 @@
 class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :image
+  attributes :id, :title, :description, :image,
+             :updated_at, :created_at
 end
