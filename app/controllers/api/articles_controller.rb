49fb86b45e453ded23ecc09de7f8491f1047832d
Dmitrijs Vrublevskis <d.vrublevskis@gmail.com>
2014-02-09 13:49:29 +0200

    Add articles

---
+++ app/controllers/api/articles_controller.rb
@@ -0,0 +1,11 @@
+class Api::ArticlesController < ApplicationController
+
+  def index
+    render json: Article.all
+  end
+
+  def show
+    render json: Article.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ LvougAppWebservice::Application.routes.draw do
 
   namespace :api do
     resources :events, only: [:index, :show]
+    resources :articles, only: [:index, :show]
   end
 
   root "rails_admin/main#dashboard"
