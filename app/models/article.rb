4852e33e14d965f72b8f7c314649bc7b956c1955
Dmitrijs Vrublevskis <d.vrublevskis@gmail.com>
2014-03-15 01:24:47 +0200

    Update event & article description limit

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: {minimum: 3, maximum: 255}
-  validates :description, presence: true, length: {minimum: 3, maximum: 1000}
+  validates :description, presence: true, length: {minimum: 3, maximum: 5000}
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
   validates :logo, presence: true, length: {minimum: 3, maximum: 255}
   validates :title, presence: true, length: {minimum: 3, maximum: 255}
-  validates :description, presence: true, length: {minimum: 3, maximum: 1000}
+  validates :description, presence: true, length: {minimum: 3, maximum: 5000}
   validates :address, presence: true, length: {minimum: 3, maximum: 255}
   validates :event_page, presence: true, length: {minimum: 3, maximum: 255}
   validates :address_latitude, numericality: true, allow_nil: true
